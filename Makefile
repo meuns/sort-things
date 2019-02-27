f38157df29ab6b81d9bdae84e8bd8a2dd835829e
Sylvain Meunier <sylvain.meunier@gmail.com>
2019-02-27 13:35:35 +0100

    First insert sort version

--- Makefile
+++ Makefile
@@ -1,17 +1,23 @@
-CC=clang
+CC='/c/Program Files/LLVM/bin/clang'
 CFLAGS=-W -Wall -std=c11 -pedantic
 LDFLAGS=
 
-all: bubble
+all: bubble insert
 
-bubble: bubble_sort.o main.o
+bubble: bubble_sort.o bubble_main.o
 	$(CC) -o $@ $^ $(LDFLAGS)
 
-main.o: bubble_sort.h main.c
+bubble_main.o: bubble_sort.h bubble_main.c
 bubble_sort.o: bubble_sort.h bubble_sort.c
+
+insert: insert_sort.o insert_main.o
+	$(CC) -o $@ $^ $(LDFLAGS)
+
+insert_main.o: insert_sort.h insert_main.c
+insert_sort.o: insert_sort.h insert_sort.c
 
 %.o: %.c
 	$(CC) -o $@ -c $< $(CFLAGS)
 
 clean:
-	rm -rf bubble *.o
+	rm -rf bubble insert *.o
--- insert_sort.c
+++ insert_sort.c
@@ -1,36 +1,43 @@
 #include "insert_sort.h"
 
-static int insert_find(int* keys, int current_index, int current_key)
+__attribute__((always_inline))
+static int insert_find_index(int* keys, int insert_index, int insert_key)
 {
-  int insert_index = current_index;
-  int insert_key = keys[insert_index - 1];
+  int compare_index = insert_index - 1;
+  int compare_key = keys[compare_index];
   
-  while (current_key < insert_key)
+  while (insert_key < compare_key && compare_index >= 0)
   {
-    --insert_index;
-    insert_key = keys[insert_index - 1];
+    compare_index = compare_index - 1;
+    compare_key = keys[compare_index];
   }
+  
+  insert_index = compare_index + 1;
 
   return insert_index;
 }
 
-static void insert_move(int* keys, int insert_index, int key_count)
+__attribute__((always_inline))
+static void insert_shift_keys(int* keys, int insert_index, int current_index)
 {
-  for (int current_index = insert_index + key_count; current_index > insert_index; --current_index)
+  int to_index = current_index;
+  int from_index = current_index - 1;
+  
+  while (to_index > insert_index)
   {
-    keys[current_index] = keys[current_index- 1];
+    keys[to_index] = keys[from_index];
+    to_index = from_index;
+    from_index = from_index - 1;
   }
 }
 
 void insert_sort(int* keys, int key_index, int key_count)
 {
-  for (int insert_index = key_index + 1; insert_index < key_count; ++insert_index)
+  for (int current_index = key_index + 1; current_index < key_count; ++current_index)
   {
-    int max_key = keys[max_index - 1];
-    int insert_key = keys[insert_index];
-    int insert_index = insert_find(keys, current_index, current_key);
-    insert_shift_keys(keys, insert_index, key_count);
-    keys[
+    const int insert_key = keys[current_index];
+    const int insert_index = insert_find_index(keys, current_index, insert_key);    
+    insert_shift_keys(keys, insert_index, current_index);
+    keys[insert_index] = insert_key;
   }
 }
-
