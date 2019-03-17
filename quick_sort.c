d10445525edea143b3f14a7cd8d6ac78fe48dcc1
Sylvain Meunier <sylvain.meunier@gmail.com>
2019-03-17 15:34:34 +0100

    Dutch flag optimisation

--- quick_sort.c
+++ quick_sort.c
@@ -1,4 +1,5 @@
 #include "quick_sort.h"
+
 #include <stdio.h>
 
 // Temporary workaround linking issue on _WIN32
@@ -50,8 +51,15 @@ WA_INLINE int quick_median3_pivot_index(int* keys, const int key_count)
   return mid_index;
 }
 
+typedef struct
+{
+  int left_dutch_index;
+  int right_dutch_index;
+}
+quick_partition_result_t;
+
 __attribute__((always_inline))
-static inline int quick_partition(int* keys, const int key_count, const int pivot_index, const int pivot_key)
+static inline quick_partition_result_t quick_partition(int* keys, const int key_count, const int pivot_index, const int pivot_key)
 {
   keys[pivot_index] = keys[0];
 
@@ -105,11 +113,30 @@ static inline int quick_partition(int* keys, const int key_count, const int pivo
   keys[0] = keys[new_pivot_index];
   keys[new_pivot_index] = pivot_key;
 
-  return new_pivot_index;
+  left_index = new_pivot_index;
+  left_key = pivot_key;
+  while (left_key == pivot_key && left_index > 0)
+  {
+    left_index = left_index - 1;
+    left_key = keys[left_index];
+  }
+
+  right_index = new_pivot_index;
+  right_key = pivot_key;
+  while (right_key == pivot_key && right_index < key_count)
+  {
+    right_index = right_index + 1;
+    right_key = keys[right_index];
+  }
+
+  quick_partition_result_t result;
+  result.left_dutch_index = left_index;
+  result.right_dutch_index = right_index;
+  return result;
 }
 
 __attribute__((noinline))
-void quick_sort(int* base_keys, const int key_count, quick_pivot_index_t quick_pivot_index)
+void quick_sort(int* base_keys, const int key_count, const quick_pivot_index_t quick_pivot_index)
 {
   const int stack_size = 32;
   int* stack_keys[stack_size];
@@ -128,9 +155,10 @@ void quick_sort(int* base_keys, const int key_count, quick_pivot_index_t quick_p
 
     const int pivot_index = quick_pivot_index(keys, key_count);
     const int pivot_key = keys[pivot_index];
-    const int partition_index = quick_partition(keys, key_count, pivot_index, pivot_key);
+    
+    const quick_partition_result_t result = quick_partition(keys, key_count, pivot_index, pivot_key);
 
-    const int left_key_count = partition_index;
+    const int left_key_count = result.left_dutch_index + 1;
     if (left_key_count > 1)
     {
       stack_top = stack_top + 1;
@@ -138,11 +166,11 @@ void quick_sort(int* base_keys, const int key_count, quick_pivot_index_t quick_p
       stack_key_count[stack_top] = left_key_count;
     }
 
-    const int right_key_count = key_count - (left_key_count + 1);
+    const int right_key_count = key_count - result.right_dutch_index;
     if (right_key_count > 1)
     {
       stack_top = stack_top + 1;
-      stack_keys[stack_top] = keys + (left_key_count + 1);
+      stack_keys[stack_top] = keys + result.right_dutch_index;
       stack_key_count[stack_top] = right_key_count;
     }
   }
