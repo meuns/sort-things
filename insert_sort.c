c5d37917f0835c33bb9e639fe8f31f0366436259
Sylvain Meunier <sylvain.meunier@gmail.com>
2019-03-26 21:42:21 +0100

    Rewrite insert sort

--- insert_sort.c
+++ insert_sort.c
@@ -1,32 +1,18 @@
 #include "insert_sort.h"
 
 __attribute__((always_inline))
-static inline int insert_find_index(int* const keys, const int insert_index, const int insert_key)
+static inline int insert_find_index(int* const keys, int insert_index, const int insert_key)
 {
-  int compare_index = insert_index - 1;
-  int compare_key = keys[compare_index];
-  
-  while (insert_key < compare_key && compare_index >= 0)
+  int compare_key = keys[insert_index - 1];
+
+  while (insert_key < compare_key && insert_index > 0)
   {
-    compare_index = compare_index - 1;
-    compare_key = keys[compare_index];
+    keys[insert_index] = compare_key;
+    insert_index = insert_index - 1;
+    compare_key = keys[insert_index - 1];
   }
 
-  return compare_index + 1;
-}
-
-__attribute__((always_inline))
-static inline void insert_shift_keys(int* keys, const int insert_index, const int current_index)
-{
-  int to_index = current_index;
-  int from_index = current_index - 1;
-  
-  while (to_index > insert_index)
-  {
-    keys[to_index] = keys[from_index];
-    to_index = from_index;
-    from_index = from_index - 1;
-  }
+  return insert_index;
 }
 
 __attribute__((noinline))
@@ -35,8 +21,7 @@ void insert_sort(int* keys, const int key_count)
   for (int current_index = 1; current_index < key_count; ++current_index)
   {
     const int insert_key = keys[current_index];
-    const int insert_index = insert_find_index(keys, current_index, insert_key);    
-    insert_shift_keys(keys, insert_index, current_index);
+    const int insert_index = insert_find_index(keys, current_index, insert_key);
     keys[insert_index] = insert_key;
   }
 }
