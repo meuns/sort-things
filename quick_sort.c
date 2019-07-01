314d3ecb039e8e7b1d720010efedf1cc6d84cae7
Sylvain Meunier <sylvain.meunier@gmail.com>
2019-07-01 18:06:56 +0200

    Use some iterators for quick sort

--- quick_sort.c
+++ quick_sort.c
@@ -8,133 +8,120 @@
 #endif
 
 __attribute__((always_inline))
-WA_INLINE int quick_middle_pivot_index(int* keys __attribute__((unused)), const int key_count)
+WA_INLINE int* quick_middle_pivot(int* const keys_begin, int* const keys_end)
 {
-  return key_count >> 1;
+  return keys_begin + ((keys_end - keys_begin) >> 1);
 }
 
 __attribute__((always_inline))
-WA_INLINE int quick_median3_pivot_index(int* keys, const int key_count)
+WA_INLINE int* quick_median3_pivot(int* const keys_begin, int* const keys_end)
 {
-  int low_index = 0;
-  int mid_index = key_count >> 1;
-  int high_index = key_count - 1;
-  
-  int low_key = keys[low_index];
-  int mid_key = keys[mid_index];    
-  int high_key = keys[high_index];
+  int* low_key_it = keys_begin;
+  int* mid_key_it = quick_middle_pivot(keys_begin, keys_end);
+  int* high_key_it = keys_end - 1;
 
-  if (mid_key > high_key)
+  if (*mid_key_it > *high_key_it)
   {
-    const int temp_key = mid_key;
-    mid_key = high_key;
-    high_key = temp_key;
-
-    const int temp_index = mid_index;
-    mid_index = high_index;
-    high_index = temp_index;
+    int* temp = mid_key_it;
+    mid_key_it = high_key_it;
+    high_key_it = temp;
   }
 
-  if (low_key > high_key)
+  if (*low_key_it > *high_key_it)
   {
-    low_key = high_key;    
-    low_index = high_index;
+    low_key_it = high_key_it;
   }
 
-  if (low_key > mid_key)
+  if (*low_key_it > *mid_key_it)
   {
-    mid_index = low_index;
+    mid_key_it = low_key_it;
   }
 
-  return mid_index;
+  return mid_key_it;
 }
 
 typedef struct
 {
-  int left_dutch_index;
-  int right_dutch_index;
+  int* left_dutch_end;
+  int* right_dutch_begin;
 }
 quick_partition_result_t;
 
 __attribute__((always_inline))
-static inline quick_partition_result_t quick_partition(int* keys, const int key_count, const int pivot_index, const int pivot_key)
+static inline quick_partition_result_t quick_partition(int* const keys_begin, int* const keys_end, int* const pivot_key_it)
 {
-  keys[pivot_index] = keys[0];
+  const int pivot_key = *pivot_key_it;
+  *pivot_key_it = *keys_begin;
 
-  int left_index = 1;
-  int left_key = keys[left_index];
+  int* left_it = keys_begin + 1;
+  int* right_it = keys_end - 1;
   
-  int right_index = key_count - 1;
-  int right_key = keys[right_index];
-  
-  while (right_key >= pivot_key && right_index > left_index)
+  while (*right_it >= pivot_key && right_it > left_it)
   {
-    right_index = right_index - 1;
-    right_key = keys[right_index];
+    right_it--;
   }
   
-  while (left_key < pivot_key && right_index > left_index)
+  while (*left_it < pivot_key && right_it > left_it)
   {
-    left_index = left_index + 1;
-    left_key = keys[left_index];      
+    left_it++;
   }
 
-  while (left_index < right_index)
+  while (left_it < right_it)
   {
-    keys[right_index] = left_key;    
-    keys[left_index] = right_key;
+    int right_key = *right_it;
+    *right_it = *left_it;
+    *left_it = right_key;
 
-    right_index = right_index - 1;
-    right_key = keys[right_index];
-    left_index = left_index + 1;
-    left_key = keys[left_index];
+    right_it--;
+    left_it++;
 
-    while (right_key >= pivot_key && right_index > left_index)
+    while (*right_it >= pivot_key && right_it > left_it)
     {
-      right_index = right_index - 1;
-      right_key = keys[right_index];
+      right_it--;
     }
     
-    while (left_key < pivot_key && right_index > left_index)
+    while (*left_it < pivot_key && right_it > left_it)
     {
-      left_index = left_index + 1;
-      left_key = keys[left_index];      
+      left_it++;
     }
   }
 
-  int new_pivot_index = right_index;    
-  if (right_key >= pivot_key && new_pivot_index > 0)
+  int* new_pivot_it = right_it;
+  if (*right_it >= pivot_key && new_pivot_it > keys_begin)
   {
-    new_pivot_index = new_pivot_index - 1;
+    new_pivot_it--;
   }
 
-  keys[0] = keys[new_pivot_index];
-  keys[new_pivot_index] = pivot_key;
+  keys_begin[0] = *new_pivot_it;
+  *new_pivot_it = pivot_key;
 
-  left_index = new_pivot_index;
-  left_key = pivot_key;
-  while (left_key == pivot_key && left_index > 0)
+  left_it = new_pivot_it;
+  while (*left_it == pivot_key && left_it > keys_begin)
   {
-    left_index = left_index - 1;
-    left_key = keys[left_index];
+    left_it--;
   }
 
-  right_index = new_pivot_index;
-  right_key = pivot_key;
-  while (right_key == pivot_key && right_index < key_count)
+  right_it = new_pivot_it;
+  while (*right_it == pivot_key && right_it < keys_end)
   {
-    right_index = right_index + 1;
-    right_key = keys[right_index];
+    right_it++;
   }
 
   quick_partition_result_t result;
-  result.left_dutch_index = left_index;
-  result.right_dutch_index = right_index;
+  result.left_dutch_end = left_it + 1;
+  result.right_dutch_begin = right_it;
   return result;
 }
 
-__attribute__((noinline))
-void quick_sort(int* base_keys, const int base_key_count, const quick_pivot_index_t quick_pivot_index)
+typedef struct
+{
+  int* keys_begin;
+  int* keys_end;
+}
+stack_element_t;
+
+__attribute__((always_inline))
+void quick_sort(int* const base_keys, const int base_key_count, const quick_pivot_t quick_pivot)
 {
   if (base_key_count < 2)
   {
@@ -142,57 +129,45 @@ void quick_sort(int* base_keys, const int base_key_count, const quick_pivot_inde
   }
 
   const int stack_size = 32;
-  int* stack_keys[stack_size];
-  int stack_key_count[stack_size];
-  int stack_top = -1;
+  stack_element_t stack[stack_size];
+  const stack_element_t* const stack_begin = stack;
+  stack_element_t* stack_top_it = stack;
+  *stack_top_it = (stack_element_t){base_keys, base_keys + base_key_count};
   
-  stack_top = stack_top + 1;
-  stack_keys[stack_top] = base_keys;
-  stack_key_count[stack_top] = base_key_count;
-  
-  while (stack_top > -1)
+  while (stack_top_it >= stack_begin)
   {
-    int* keys = stack_keys[stack_top];
-    const int key_count = stack_key_count[stack_top];
-    stack_top = stack_top - 1;
+    const stack_element_t stack_element = *stack_top_it--;
+    int* const keys_begin = stack_element.keys_begin;
+    int* const keys_end = stack_element.keys_end;
 
-    const int pivot_index = quick_pivot_index(keys, key_count);
-    const int pivot_key = keys[pivot_index];
-    
-    const quick_partition_result_t result = quick_partition(keys, key_count, pivot_index, pivot_key);
-    const int left_key_count = result.left_dutch_index + 1;
-    const int right_key_count = key_count - result.right_dutch_index;
+    const quick_partition_result_t result = quick_partition(keys_begin, keys_end, quick_pivot(keys_begin, keys_end));
+    int* const left_dutch_end = result.left_dutch_end;
+    int* const right_dutch_begin = result.right_dutch_begin;
+    const int left_key_count = (int)(left_dutch_end - keys_begin);
+    const int right_key_count = (int)(keys_end - right_dutch_begin);
 
     if (left_key_count > right_key_count)
     {
       if (left_key_count > 1)
       {
-        stack_top = stack_top + 1;
-        stack_keys[stack_top] = keys;
-        stack_key_count[stack_top] = left_key_count;
+        *(++stack_top_it) = (stack_element_t){keys_begin, left_dutch_end};
       }
       
       if (right_key_count > 1)
       {
-        stack_top = stack_top + 1;
-        stack_keys[stack_top] = keys + result.right_dutch_index;
-        stack_key_count[stack_top] = right_key_count;
+        *(++stack_top_it) = (stack_element_t){right_dutch_begin, keys_end};
       }
     }
     else
     {
       if (right_key_count > 1)
       {
-        stack_top = stack_top + 1;
-        stack_keys[stack_top] = keys + result.right_dutch_index;
-        stack_key_count[stack_top] = right_key_count;
+        *(++stack_top_it) = (stack_element_t){right_dutch_begin, keys_end};
       }
 
       if (left_key_count > 1)
       {
-        stack_top = stack_top + 1;
-        stack_keys[stack_top] = keys;
-        stack_key_count[stack_top] = left_key_count;
+        *(++stack_top_it) = (stack_element_t){keys_begin, left_dutch_end};
       }
     }
   }
--- quick_sort.h
+++ quick_sort.h
@@ -1,8 +1,7 @@
 #pragma once
 
-typedef int (*quick_pivot_index_t)(int* keys, const int key_count);
+typedef int* (*quick_pivot_t)(int* const keys_begin, int* const keys_end);
+int* quick_middle_pivot(int* const keys_begin, int* const keys_end);
+int* quick_median3_pivot(int* const keys_begin, int* const keys_end);
 
-int quick_middle_pivot_index(int* keys, const int key_count);
-int quick_median3_pivot_index(int* keys, const int key_count);
-
-void quick_sort(int* keys, const int key_count, quick_pivot_index_t quick_pivot_index);
+void quick_sort(int* const keys, const int key_count, const quick_pivot_t quick_pivot);
